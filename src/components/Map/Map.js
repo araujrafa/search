0f53467c5b1d88815db2a9a8ae2dc27e1b81321b
Rafael Araujo <rafael.araujo@grupofolha.com.br>
2018-04-16 15:48:17 -0300

    Adicionado Iframe do Google maps para exibir endereço

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,13 +1,21 @@
 import React from 'react';
 
-export default props => {
+export default ({ infosCep }) => {
+  const urlIframe = (cep) => `https://www.google.com.br/maps?q=${cep},%20Brasil&output=embed`;
+
   return (
     <div className="c-map">
-      <h3>{props.infosCep.logradouro}</h3>
-      <p>{props.infosCep.bairro}</p>
-      <p>{props.infosCep.localidade} - {props.infosCep.uf}</p>
-      <p>{props.infosCep.cep}</p>
-      MAPA
+      <h3>{infosCep.logradouro}</h3>
+      <p>{infosCep.bairro}</p>
+      <p>{infosCep.localidade} - {infosCep.uf}</p>
+      <p>{infosCep.cep}</p>
+      <iframe
+        title="GoogleMaps"
+        width="600"
+        height="450"
+        frameborder="0" style={{border:0}}
+        src={urlIframe(infosCep.cep)} allowfullscreen>
+      </iframe>
     </div>
   )
 }
