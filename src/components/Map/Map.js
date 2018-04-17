eb18e253302e6f998ede1fde77a1a84229362a42
Rafael Araujo <rafael.araujo@grupofolha.com.br>
2018-04-17 09:55:14 -0300

    Adicionado biblioteca de mascara

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,21 +1,21 @@
 import React from 'react';
 
 export default ({ infosCep }) => {
-  const urlIframe = (cep) => `https://www.google.com.br/maps?q=${cep},%20Brasil&output=embed`;
-
   return (
     <div className="c-map">
       <h3>{infosCep.logradouro}</h3>
       <p>{infosCep.bairro}</p>
-      <p>{infosCep.localidade} - {infosCep.uf}</p>
+      <p>{infosCep.uf ? `${infosCep.localidade} - ${infosCep.uf}` : infosCep.localidade}</p>
       <p>{infosCep.cep}</p>
       <iframe
         title="GoogleMaps"
         width="600"
         height="450"
-        frameborder="0" style={{border:0}}
-        src={urlIframe(infosCep.cep)} allowfullscreen>
-      </iframe>
+        frameBorder="0" 
+        style={{border:0}}
+        src={`https://www.google.com.br/maps?q=${infosCep.cep},%20Brasil&output=embed`} 
+        allowFullScreen
+        />
     </div>
   )
 }
--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,17 +1,19 @@
 import React from 'react';
+import MaskedInput from 'react-maskedinput';
 
 export default props => {
   const keyHandler = (e) => {
     if (e.key === 'Enter') {
-      props.onSubmit()
+      props.onSubmit();
     }
   }
 
   return (
     <div className="c-search">
       <label>CEP</label>
-      <input
+      <MaskedInput
         type="text"
+        mask="11111-111"
         value={props.cep}
         onChange={props.onChange}
         onKeyUp={keyHandler}
