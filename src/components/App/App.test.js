c07e4b0af7e3fd1995ca0e31dda2ba227b3267f3
Rafael Monteiro <rafael.araujo@grupofolha.com.br>
2018-04-19 22:47:52 -0300

    Adicionado teste inicial do compontene app

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,15 +1,33 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import App from './App.js';
+import Search from '../Search/Search';
 
 describe('<App />', () => {
   let component;
+  let search;
 
-  beforeAll(() => {
+  beforeEach(() => {
     component = shallow(<App />);
+
+    search = component.find('Search');
+    console.log(search)
   });
 
   test('Should have state default', () => {
-    // expect(component.find('<Map />').length).toBe(1);
+    expect(component.state('cep')).toBe('');
+    expect(component.state('infosCep')).toEqual([]);
+    expect(component.state('show')).toBe(false);
+    expect(component.state('error')).toBe(false);
+  });
+
+  test('Should have component Search', () => {
+    expect(search.length).toBe(1);
+  });
+
+  test('Should change prop cep in Search', () => {
+    component.setState({ cep: '08430-180' });
+    console.log(component.state('cep'))
+    expect(search.prop('cep')).toBe('08430-180');
   });
 });
